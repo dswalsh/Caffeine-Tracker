eab00ad8eba4e9e041adafe23086db002a4f5ad8
Daniel Walsh <dwalsh@Daniels-iMac.local>
2013-10-24 21:56:26 -0400

    Initial Commit

---
+++ Caffeine Tracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Caffeine Tracker
+//
+//  Created by Daniel Walsh on 10/24/13.
+//  Copyright (c) 2013 Daniel Walsh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CTAppDelegate class]));
+    }
+}
---
+++ Caffeine TrackerTests/Caffeine_TrackerTests.m
@@ -0,0 +1,34 @@
+//
+//  Caffeine_TrackerTests.m
+//  Caffeine TrackerTests
+//
+//  Created by Daniel Walsh on 10/24/13.
+//  Copyright (c) 2013 Daniel Walsh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Caffeine_TrackerTests : XCTestCase
+
+@end
+
+@implementation Caffeine_TrackerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
